4760ee385c8937847da4a455a7a562d788e333a6
Cassia Wagner <cassia.wagner9@gmail.com>
2019-08-13 15:40:58 -0700

    Updates scripts to store genetic distance by segment.

--- scripts/compare.py
+++ scripts/compare.py
@@ -1,6 +1,10 @@
 '''
-This script takes an alignment of sequences and produces a dictionary containing
-pairwise divergence and location of all SNPs for all possible sequence pairs.
+This scripts finds pairwise divergence for each sample pair in an alignment.
+Currently, it works for influenza alignments given in the order: 'ha', 'na', 'pb2', 'pb1' 'pa', 'np', 'mp', 'ns'.
+
+Its inputs are:
+    --alignments, a list of genome segment alignments
+    --output, the location of an output dictionary.
 '''
 
 import argparse
@@ -9,126 +13,113 @@ import numpy as np
 from itertools import combinations
 import pickle
 
-def shared_strains(align_list):
-    """
-    Return list of strains shared across all genome segments.
-    """
+def shared(align_list):
+    '''
+    Returns list of strains shared across all genome segments.
+    '''
     setlist = []
-    for align in align_list:
+    for alignment in align_list:
         strains = set()
-        for record in SeqIO.parse(align, "fasta"):
+        for record in SeqIO.parse(alignment, 'fasta'):
             strains.add(record.id)
         setlist.append(strains)
     shared = set.intersection(*setlist)
     return shared
 
-    return mapping, genome_length
-
-def concat(shared, align_list):
-    """
-    Concatenates genome segments together
-    """
+def convert(align_list, segments, shared):
+    '''
+    It replaces nucleotide bases with numbers in order to find SNPs efficiently. C,T,G,A are replaced with the first 4 prime numbers.
+    Ambiguous nucleotide bases are replaced with the multiple of those primes for whichever bases the ambiguous bases could represent.
+    For example, R, which represents A or G, is 35 because A is 7, and G is 5.
+    '''
     mapping = {}
     for strain in shared:
-        mapping[strain] = ""
-    for align in align_list:
-        for record in SeqIO.parse(align, "fasta"):
+        mapping[strain] = {}
+    for alignment, segment in zip(align_list, segments):
+        for record in SeqIO.parse(alignment, 'fasta'):
             if record.id in mapping:
-                seq = str(record.seq)
-                mapping[record.id] += seq
-    return mapping
-
-def convert(genome_dict):
-    """
-    It replaces nucleotide bases with numbers in order to find SNPs efficiently computationally and deal with ambiguous nucleotide bases.
-    C,T,G,A are replaced with the first 4 prime numbers. Ambiguous nucleotide bases are replaced with the multiple of the prime numbers
-    for whichever bases it can represent. For example, R, which could be A or G, is 35 because A is 7, and G is 5.
-    """
-    mapping = {}
-    for record in genome_dict:
-        seq = list(genome_dict[record])
-        seq_conversion = {
-            "C": 2,
-            "T": 3,
-            "G": 5,
-            "A": 7,
-            "D": 105,
-            "R": 35,
-            "Y": 6,
-            "H": 42,
-            "K": 15,
-            "V": 70,
-            "N": 210,
-            "M": 14,
-            "W": 21,
-            "S": 10
-        }
-        sequence = [seq_conversion.get(base) for base in seq]
-        array = np.asarray(sequence, dtype=int)
-        mapping[record] = array
+                seq = list(record.seq)
+                seq_conversion = {
+                    'C': 2,
+                    'T': 3,
+                    'G': 5,
+                    'A': 7,
+                    'D': 105,
+                    'R': 35,
+                    'Y': 6,
+                    'H': 42,
+                    'K': 15,
+                    'V': 70,
+                    'N': 210,
+                    'M': 14,
+                    'W': 21,
+                    'S': 10
+                }
+                sequence = [seq_conversion.get(base) for base in seq]
+                array = np.asarray(sequence, dtype=int)
+                mapping[record.id][segment] = array
     return mapping
 
 def compare(array1, array2):
+    '''
+    This script identifies SNPs between sequences where sequences are represented as numpy integer arrays.
+    CTGA must be represented by prime numbers and ambiguous bases are the multiples of those prime numbers.
+    '''
     condition1 = (array1 != array2)
     condition2 = (array1 % array2 != 0)
     condition3 = (array2 % array1 != 0)
     location = np.where(condition1 & condition2 & condition3)
     return location[0]
 
-def pairwise(mapping):
-    """
-    Compares things pairwise
-    """
+def pairwise(mapping, segments):
+    '''
+    Compares all strains pairwise storing the genetic distance between each strain and the location of pairwise SNPs.
+    '''
     counter = 0
-    interval = 5000
+    interval = 50000
     pairwise_dict = {}
     strain_pairs = list(combinations(mapping.keys(), 2))
     for pair in strain_pairs:
+        pairwise_dict[pair] = {}
         if counter % interval == 0:
-            print("[", end="")
+            print('[', end='')
             for x in range(int(counter / interval)):
-                print("-", end="")
-            for x in range(int(len(strain_pairs) / interval) - int(counter / interval)):
-                print(" ", end="")
-            print("]")
-        genome_length = len(mapping[pair[0]])
-        location = compare(mapping[pair[0]], mapping[pair[1]])
-        distance = np.size(location)
-        pairwise_dict[pair] = {
-            "snp_loc": location,
-            "distance": distance,
-            "length": genome_length
-        }
-        counter += 1
+                print('-', end='')
+            for x in range(int(len(strain_pairs)*len(segments) / interval) - int(counter / interval)):
+                print(' ', end='')
+            print(']')
+        for segment in segments:
+            location = compare(mapping[pair[0]][segment], mapping[pair[1]][segment])
+            distance = np.size(location)
+            pairwise_dict[pair][segment] = {
+                'snp_loc': location,
+                'distance': distance,
+            }
+            counter += 1
     return pairwise_dict
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="Pairwise analysis for each sequence.",
+        description='Pairwise analysis for each sequence.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    parser.add_argument('--alignment', type=str, nargs='+', required=True, help="list of alignments")
-    parser.add_argument('--shared', type=str, required=True, help = "name of output of shared strains")
-    parser.add_argument('--dictionary', type=str, required=True, help = "name of output dictionary")
+    parser.add_argument('--alignments', type=str, nargs='+', required=True, help='alignment for viruses')
+    parser.add_argument('--output', type=str, required=True, help = 'name of output dictionary')
     args = parser.parse_args()
 
-    #Create list of strains shared across all segments
-    shared = shared_strains(args.alignment)
+    # Define influenza segments.
+    segments = ['ha', 'na', 'pb2', 'pb1', 'pa', 'np', 'mp', 'ns']
 
-    #Save shared strains as pickle files
-    with open(args.shared, 'wb') as file:
-        pickle.dump(shared, file)
+    # Identifies set of strains shared across all segments.
+    shared_strains = shared(args.alignments)
 
-    #Concatenate segments into one genome
-    concatenated = concat(shared, args.alignment)
+    # Converts bases from letters to numbers for comparison
+    converted = convert(args.alignments, segments, shared_strains)
 
-    #Converts bases from letters to numbers for comparison
-    converted = convert(concatenated)
+    # Calculates pairwise divergence for every strain
+    comparison_dict = pairwise(converted, segments)
 
-    #Calculates pairwise divergence for every strain
-    comparison_dict = pairwise(converted)
-
-    #Save dictionary as pickle file.
-    with open(args.dictionary, 'wb') as file:
+    # Save dictionary as pickle file.
+    with open(args.output, 'wb') as file:
         pickle.dump(comparison_dict, file)
--- scripts/plot.py
+++ scripts/plot.py
@@ -1,8 +1,22 @@
+'''
+This script plots histograms of pairwise divergence for the full influenza genome and each influenza genome segment.
+Histograms are colored for within cluster pairs and between cluster pairs for clusters based on specific genetic distance cutoff.
+
+Inputs are:
+    --pairwise, a pickle dictionary of the format: {
+        (strainA, strainB) : {
+        segment : {
+        'distance' : 2,
+        'snp_loc' : 27, 32
+        }}}
+    --cutoff, Hamming distance at which to cluster samples
+    --output-genome, name of PNG for full genome histogram
+    --output-segments, name of PNG for segments histogram
+'''
 
 import argparse
 import pickle
 import numpy as np
-from itertools import combinations
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import connected_components
 import random
@@ -10,162 +24,127 @@ from scipy import stats
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
-def cluster(shared, pairwise_dict, cutoff):
-    adj_matrix = np.zeros((len(shared), len(shared)))
-    for indexA, strainA in enumerate(shared):
-        for indexB, strainB in enumerate(shared):
-            if indexA != indexB:
-                if (strainA, strainB) in pairwise_dict:
-                    if pairwise_dict[(strainA, strainB)]['distance'] < cutoff:
-                        adj_matrix[indexA, indexB] = 1
-                        adj_matrix[indexB, indexA] = 1
-                elif pairwise_dict[(strainB, strainA)]['distance'] < cutoff:
-                    adj_matrix[indexA, indexB] = 1
-                    adj_matrix[indexB, indexA] = 1
+def strains(pairwise):
+    '''
+    Returns set of the strains compared in pairwise dictionary.
+    '''
+    strain_list = set()
+    for (strainA, strainB) in pairwise.keys():
+        strain_list.add(strainA)
+        strain_list.add(strainB)
+    return strain_list
 
+def adjacency_matrix(strain_list, pairwise, segments, cutoff):
+    '''
+    Returns an adjacency matrix on which to cluster.
+    '''
+    adj_matrix = np.zeros((len(strain_list), len(strain_list)))
+    for indexA, strainA in enumerate(strain_list):
+        for indexB, strainB in enumerate(strain_list):
+            if indexA != indexB:
+                for segment in segments:
+                    if (strainA, strainB) in pairwise:
+                        adj_matrix[indexA, indexB] += pairwise[(strainA, strainB)][segment]['distance']
+                        adj_matrix[indexB, indexA] += pairwise[(strainA, strainB)][segment]['distance']
+                    elif (strainB, strainA) in pairwise:
+                        adj_matrix[indexA, indexB] += pairwise[(strainB, strainA)][segment]['distance']
+                        adj_matrix[indexB, indexA] += pairwise[(strainB, strainA)][segment]['distance']
+    adj_matrix[adj_matrix < cutoff] = 1
+    adj_matrix[adj_matrix >= cutoff] = 0
+    return adj_matrix
+
+def cluster(adj_matrix, strain_list):
+    '''
+    Uses SciPy connected_components to cluster samples.
+    '''
     graph = csr_matrix(adj_matrix)
     n_clusters, labels = connected_components(
         csgraph=graph, directed=False, return_labels=True
     )
-
-    cluster_dict =  dict(zip(shared, labels))
+    cluster_dict =  dict(zip(strain_list, labels))
     return cluster_dict
 
 def zscore(data):
+    '''
+    Returns z-score.
+    '''
     z = np.abs(stats.zscore(data))
     return z
 
-def list_distances(pairwise_dict, cluster_dict):
-    keys = list(random.sample(pairwise_dict.keys(), 10000))
+def distances(pairwise, segments, cluster_dict):
+    '''
+    Creates dictionary containing key : list of genetic distances for 10,000 random pairs of samples.
+    Keys include 'all', 'between_cluster', 'within_cluster', and within & between for each influenza segment.
+    '''
+    keys = random.sample(pairwise.keys(), 10000)
     distances = {}
-    distances['all_dist'] = []
-    distances['within_cluster'] = []
-    distances['between_cluster'] = []
-    distances['ha'] = []
-    distances['ha_within'] = []
-    distances['ha_between'] = []
-    distances['na'] = []
-    distances['na_within'] = []
-    distances['na_between'] = []
-    distances['pb2'] = []
-    distances['pb2_within'] = []
-    distances['pb2_between'] = []
-    distances['pb1'] = []
-    distances['pb1_within'] = []
-    distances['pb1_between'] = []
-    distances['pa'] = []
-    distances['pa_within'] = []
-    distances['pa_between'] = []
-    distances['np'] = []
-    distances['np_within'] = []
-    distances['np_between'] = []
-    distances['mp'] = []
-    distances['mp_within'] = []
-    distances['mp_between'] = []
-    distances['ns'] = []
-    distances['ns_within'] = []
-    distances['ns_between'] = []
+    all_dist = {}
+    within_cluster = {}
+    between_cluster = {}
+    for segment in segments:
+        distances[segment + '_within'] = []
+        distances[segment + '_between'] = []
 
     for key in keys:
-        distances['all_dist'].append(pairwise_dict[key]['distance'])
-        ha_location = [loc for loc in pairwise_dict[key]['snp_loc'] if loc < 1702]
-        ha_distance = np.size(ha_location)
-        distances['ha'].append(ha_distance)
-        na_location = [loc for loc in pairwise_dict[key]['snp_loc'] if loc >= 1702 & loc < 3138]
-        na_distance = np.size(na_location)
-        distances['na'].append(na_distance)
-        pb2_location = [loc for loc in pairwise_dict[key]['snp_loc'] if loc >= 3138 & loc < 5448]
-        pb2_distance = np.size(pb2_location)
-        distances['pb2'].append(pb2_distance)
-        pb1_location = [loc for loc in pairwise_dict[key]['snp_loc'] if loc >=5448 & loc < 7759]
-        pb1_distance = np.size(pb1_location)
-        distances['pb1'].append(pb1_distance)
-        pa_location = [loc for loc in pairwise_dict[key]['snp_loc'] if loc >= 7759 & loc < 9951]
-        pa_distance = np.size(pa_location)
-        distances['pa'].append(pa_distance)
-        np_location = [loc for loc in pairwise_dict[key]['snp_loc'] if loc >= 9951 & loc < 11488]
-        np_distance = np.size(np_location)
-        distances['np'].append(np_distance)
-        mp_location = [loc for loc in pairwise_dict[key]['snp_loc'] if loc >= 11488 & loc < 12487]
-        mp_distance = np.size(mp_location)
-        distances['mp'].append(mp_distance)
-        ns_location = [loc for loc in pairwise_dict[key]['snp_loc'] if loc >= 12487]
-        ns_distance = np.size(ns_location)
-        distances['ns'].append(ns_distance)
         (strainA, strainB) = key
+        all_dist[key] = 0
+        for segment in segments:
+            all_dist[key] += pairwise[key][segment]['distance']
         if cluster_dict[strainA] == cluster_dict[strainB]:
-            distances['within_cluster'].append(pairwise_dict[key]['distance'])
-            distances['ha_within'].append(ha_distance)
-            distances['na_within'].append(na_distance)
-            distances['pb2_within'].append(pb2_distance)
-            distances['pb1_within'].append(pb1_distance)
-            distances['pa_within'].append(pa_distance)
-            distances['np_within'].append(np_distance)
-            distances['mp_within'].append(mp_distance)
-            distances['ns_within'].append(ns_distance)
+            within_cluster[key] = 0
+            for segment in segments:
+                within_cluster[key] += pairwise[key][segment]['distance']
+                distances[segment + '_within'].append(pairwise[key][segment]['distance'])
         else:
-            distances['between_cluster'].append(pairwise_dict[key]['distance'])
-            distances['ha_between'].append(ha_distance)
-            distances['na_between'].append(na_distance)
-            distances['pb2_between'].append(pb2_distance)
-            distances['pb1_between'].append(pb1_distance)
-            distances['pa_between'].append(pa_distance)
-            distances['np_between'].append(np_distance)
-            distances['mp_between'].append(mp_distance)
-            distances['ns_between'].append(ns_distance)
+            between_cluster[key] = 0
+            for segment in segments:
+                between_cluster[key] += pairwise[key][segment]['distance']
+                distances[segment + '_between'].append(pairwise[key][segment]['distance'])
+    distances['all'] = list(all_dist.values())
+    distances['within'] = list(within_cluster.values())
+    distances['between'] = list(between_cluster.values())
+    return distances
 
-    # Removs outliers from data
+def remove_outliers(distance_dict):
+    '''
+    Removes outliers from distances to plot.
+    '''
     distances_edited = {}
-    for key, value in distances.items():
+    for key, value in distance_dict.items():
         z = zscore(value)
         distances_edited[key] = [distance for index, distance in enumerate(distances[key]) if z[index] < 4]
-
     return distances_edited
 
-
-def list_distances_ha(pairwise_dict, cluster_dict):
-    keys = list(random.sample(pairwise_dict.keys(), 10000))
-    distances = {}
-    distances['all_dist'] = []
-    distances['within_cluster'] = []
-    distances['between_cluster'] = []
-    for key in keys:
-        distances['all_dist'].append(pairwise_dict[key]['distance'])
-        (strainA, strainB) = key
-        if cluster_dict[strainA] == cluster_dict[strainB]:
-            distances['within_cluster'].append(pairwise_dict[key]['distance'])
-        else:
-            distances['between_cluster'].append(pairwise_dict[key]['distance'])
-
-    # Removes outliers from data. There ended up being one outlier at a genetic distance of 1750, that I think might just be the wrong sequence.
-    distances_edited = {}
-    z_all = zscore(distances['all_dist'])
-    z_within = zscore(distances['within_cluster'])
-    z_between = zscore(distances['between_cluster'])
-    distances_edited['all_dist'] = [distance for index, distance in enumerate(distances['all_dist']) if z_all[index] < 4]
-    distances_edited['within_cluster'] = [distance for index, distance in enumerate(distances['within_cluster']) if z_within[index] < 4]
-    distances_edited['between_cluster'] = [distance for index, distance in enumerate(distances['between_cluster']) if z_between[index] < 4]
-
-    return distances_edited
-
-def hist_distances(distance_dict, output_genome, output_segments):
+def hist_genome(distance_dict, output_genome):
+    '''
+    Plots histogram of genetic distance for entire flu genome
+    '''
     mpl.rcParams['font.weight']=110
     mpl.rcParams['axes.labelweight']=110
     mpl.rcParams['font.size']=14
 
-    fig1, (ax1, ax2) = plt.subplots(2, 1, figsize=(16, 12), facecolor="white")
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(16, 12), facecolor="white", sharex=True, sharey=True)
     ax1.set_title('H3N2: Full genome')
-    ax1.hist(distance_dict['all_dist'], bins=len(set(distance_dict['all_dist'])))
+    ax1.hist(distance_dict['all'], bins=len(set(distance_dict['all'])))
     ax1.set_ylabel("Frequency")
-    ax2.hist((distance_dict['within_cluster'], distance_dict['between_cluster']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['all_dist'])))
+    ax2.hist((distance_dict['within'], distance_dict['between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['all'])))
     ax2.set_xlabel("Pairwise genetic distance")
     ax2.set_ylabel("Frequency")
     ax2.legend()
+    return fig.savefig(output_genome, dpi=300)
 
-    fig2, axs = plt.subplots(4, 4, figsize=(16, 12), facecolor="white", sharex=True, sharey=True)
-    fig2.suptitle('H3N2', fontsize='large')
-    fig2.text(0.5, 0.04, 'Pairwise genetic distance', ha='center')
-    fig2.text(0.04, 0.5, 'Frequency', va='center', rotation='vertical')
+def hist_segments(distance_dict, output_segments):
+    '''
+    Plots histogram of genetic distance for each individual flu segment.
+    '''
+    mpl.rcParams['font.weight']=110
+    mpl.rcParams['axes.labelweight']=110
+    mpl.rcParams['font.size']=14
+
+    fig, axs = plt.subplots(2, 4, figsize=(16, 12), facecolor="white", sharex=True, sharey=True)
+    fig.suptitle('H3N2', fontsize='large')
+    fig.text(0.5, 0.04, 'Pairwise genetic distance', ha='center')
+    fig.text(0.04, 0.5, 'Frequency', va='center', rotation='vertical')
     ax1 = axs[0,0]
     ax2 = axs[1,0]
     ax3 = axs[0,1]
@@ -174,41 +153,25 @@ def hist_distances(distance_dict, output_genome, output_segments):
     ax6 = axs[1,2]
     ax7 = axs[0,3]
     ax8 = axs[1,3]
-    ax9 = axs[2,0]
-    ax10 = axs[3,0]
-    ax11 = axs[2,1]
-    ax12 = axs[3,1]
-    ax13 = axs[2,2]
-    ax14 = axs[3,2]
-    ax15 = axs[2,3]
-    ax16 = axs[3,3]
 
-    ax1.hist(distance_dict['ha'], label='HA', bins=len(set(distance_dict['ha'])))
     ax1.set_title('HA')
-    ax2.hist((distance_dict['ha_within'], distance_dict['ha_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['ha'])))
-    ax3.hist(distance_dict['na'], label='NA', bins=len(set(distance_dict['na'])))
-    ax3.set_title('NA')
-    ax4.hist((distance_dict['na_within'], distance_dict['na_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['na'])))
-    ax5.hist(distance_dict['pb2'], label='PB2', bins=len(set(distance_dict['pb2'])))
-    ax5.set_title('PB2')
-    ax6.hist((distance_dict['pb2_within'], distance_dict['pb2_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['pb2'])))
-    ax7.hist(distance_dict['pb1'], label='PB1', bins=len(set(distance_dict['pb1'])))
-    ax7.set_title('PB1')
-    ax8.hist((distance_dict['pb1_within'], distance_dict['pb1_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['pb1'])))
-    ax9.hist(distance_dict['pa'], label='PA', bins=len(set(distance_dict['pa'])))
-    ax9.set_title('PA')
-    ax10.hist((distance_dict['pa_within'], distance_dict['pa_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['pa'])))
-    ax11.hist(distance_dict['np'], label='NP', bins=len(set(distance_dict['np'])))
-    ax11.set_title('NP')
-    ax12.hist((distance_dict['np_within'], distance_dict['np_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['np'])))
-    ax13.hist(distance_dict['mp'], label='MP', bins=len(set(distance_dict['mp'])))
-    ax13.set_title('MP')
-    ax14.hist((distance_dict['mp_within'], distance_dict['mp_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['mp'])))
-    ax15.hist(distance_dict['ns'], label='NS', bins=len(set(distance_dict['ns'])))
-    ax15.set_title('NS')
-    ax16.hist((distance_dict['ns_within'], distance_dict['ns_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set(distance_dict['ns'])))
+    ax1.hist((distance_dict['ha_within'], distance_dict['ha_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set.union(set(distance_dict['ha_within']), set(distance_dict['ha_between']))))
+    ax2.set_title('NA')
+    ax2.hist((distance_dict['na_within'], distance_dict['na_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set.union(set(distance_dict['na_within']), set(distance_dict['na_between']))))
+    ax3.set_title('PB2')
+    ax3.hist((distance_dict['pb2_within'], distance_dict['pb2_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set.union(set(distance_dict['pb2_within']), set(distance_dict['pb2_between']))))
+    ax4.set_title('PB1')
+    ax4.hist((distance_dict['pb1_within'], distance_dict['pb1_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set.union(set(distance_dict['pb1_within']), set(distance_dict['pb1_between']))))
+    ax5.set_title('PA')
+    ax5.hist((distance_dict['pa_within'], distance_dict['pa_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set.union(set(distance_dict['pa_within']), set(distance_dict['pa_between']))))
+    ax6.set_title('NP')
+    ax6.hist((distance_dict['np_within'], distance_dict['np_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set.union(set(distance_dict['np_within']), set(distance_dict['np_between']))))
+    ax7.set_title('MP')
+    ax7.hist((distance_dict['mp_within'], distance_dict['mp_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set.union(set(distance_dict['mp_within']), set(distance_dict['mp_between']))))
+    ax8.set_title('NS')
+    ax8.hist((distance_dict['ns_within'], distance_dict['ns_between']), label = ('Within clusters', 'Between clusters'), stacked=True, bins=len(set.union(set(distance_dict['ns_within']), set(distance_dict['ns_between']))))
 
-    return fig1.savefig(output_genome, dpi=300), fig2.savefig(output_segments, dpi=300)
+    return fig.savefig(output_segments, dpi=300)
 
 
 if __name__ == '__main__':
@@ -217,26 +180,34 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    parser.add_argument('--shared', type=str, required=True, help="name of shared strains pickle file")
-    parser.add_argument('--pairwise', type=str, required=True, help="name of pairwise pickle file")
+    parser.add_argument('--pairwise', type=str, required=True, help="pairwise pickle file")
     parser.add_argument('--cutoff', type=int, required=True, help="Genomic distance cutoff on which to cluster")
     parser.add_argument('--output-genome', type=str, required=True, help = "name of output figure")
     parser.add_argument('--output-segments', type=str, required=True, help = "name of output figure")
     args = parser.parse_args()
 
-    # Loads list of shared strains
-    with open(args.shared, "rb") as file:
-        shared = pickle.load(file)
+    # Define influenza segments
+    segments = ['ha', 'na', 'pb2', 'pb1', 'pa', 'np', 'mp', 'ns']
 
-    # Loads dictionary containing pairwise divegence
+    # Loads dictionar(y/ies) containing pairwise divegence
     with open(args.pairwise, "rb") as file:
         pairwise = pickle.load(file)
 
-    # Cluster strains by genetic distance
-    clusters = cluster(shared, pairwise, args.cutoff)
+    # Lists strains shared across all segments
+    strains_list = strains(pairwise)
+
+    # Defines adjacency matrix on which to cluster
+    adj_matrix = adjacency_matrix(strains_list, pairwise, segments, args.cutoff)
+
+    # Clusters strains based on genetic distance
+    clusters = cluster(adj_matrix, strains_list)
 
     # Creates list of distances to plot
-    distances = list_distances(pairwise, clusters)
+    distance_dict = distances(pairwise, segments, clusters)
 
-    # Writes histogram of distances
-    hist_distances(distances, args.output_genome, args.output_segments)
+    # Removes outliers with a Z-score > 4 from distances dictionary.
+    distances_edited = remove_outliers(distance_dict)
+
+    # Writes histograms of distances
+    hist_genome(distances_edited, args.output_genome)
+    hist_segments(distances_edited, args.output_segments)
