fb0b111f53edb148ce94a5eb084bbec64e7ff37f
Cassia Wagner <cassia.wagner9@gmail.com>
2019-08-14 13:46:01 -0700

    Adds heatmap.py to visualize distance by segment.

---
+++ scripts/heatmap.py
@@ -0,0 +1,106 @@
+'''
+This script produces a heatmap showing genetic distance between each segment for random pairs of samples.
+The heatmap is ordered by full genome genetic distance.
+'''
+
+import argparse
+import pickle
+import random
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def load(pfile):
+    '''
+    Loads pairwise dictionary from pickle file.
+    '''
+    with open(pfile, 'rb') as file:
+        pairwise = pickle.load(file)
+    return pairwise
+
+def add_total_distance(pairwise, segments):
+    '''
+    Adds total genetic distance for each pair to the dictionary.
+    '''
+    for pair in pairwise:
+        pairwise[pair]['distance'] = 0
+        for segment in segments:
+            pairwise[pair]['distance'] += pairwise[pair][segment]['distance']
+    return pairwise
+
+def choose_random_pairs(pairwise, max_distance):
+    '''
+    Chooses a random pair of samples with a full-genome genetic distance of X for every X in range(0,max_distance)
+    '''
+    iterate_list = [*range(max_distance)]
+    pair_list = []
+    for num in iterate_list:
+        distance = []
+        for pair in pairwise:
+            if pairwise[pair]['distance'] == num:
+                distance.append(pair)
+        pair_list.append(distance)
+    pairs = []
+    for distance in pair_list:
+        pairs.append(random.choice(distance))
+    return pairs
+
+def pairs_to_array(max_distance, segments, pair_list, pairwise):
+    '''
+    Creates a matrix where each row represents is a pairwise, and each column is the genetic distance between each segment.
+    Rows are ordered from 0 to max_distance.
+    '''
+    array = np.zeros((max_distance, len(segments)), dtype=int)
+    for pair, row in zip(pair_list, range(max_distance)):
+        for segment, value in zip(segments, range(len(segments))):
+            array[row, value] = pairwise[pair][segment]['distance']
+    return array
+
+def heatmap(array, segments, max_distance, output):
+    '''
+    Plots array as a heatmap.
+    '''
+    mpl.rcParams['font.weight']=200
+    mpl.rcParams['axes.labelweight']=200
+    mpl.rcParams['font.size']=14
+
+    fig, ax = plt.subplots(figsize=(8, 10), facecolor='white')
+    image = ax.pcolormesh(array)
+    cbar = fig.colorbar(image, ax=ax)
+    cbar.set_label('Genetic distance\nper segment')
+
+    ax.set_xticks(np.arange(len(segments))+0.5)
+    ax.set_yticklabels(range(0, max_distance, 50))
+    ax.set_xticklabels(segments, ha = 'center')
+    ax.set_ylabel('Pairwise genetic distance')
+
+    return plt.savefig(output, dpi=250)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Heatmap of genetic distance for influenza',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('--pairwise', type=str, required=True, help='pairwise pickle file')
+    parser.add_argument('--max_distance', type=int, required=True, help='Maximum distance for heatmap')
+    parser.add_argument('--output', type=str, required=True, help = 'name of output figure')
+    args = parser.parse_args()
+
+    # Define influenza segments
+    segments = ['ha', 'na', 'pb2', 'pb1', 'pa', 'np', 'mp', 'ns']
+
+    # Loads comparison dictionary from pickle file
+    pairwise = load(args.pairwise)
+
+    # Adds total distance to pairwise dictionary
+    pairwise_updated = add_total_distance(pairwise, segments)
+
+    # Chooses random pairs for every distance from 0, max_distance
+    random_pairs = choose_random_pairs(pairwise_updated, args.max_distance)
+
+    # Creates array to plot.
+    array = pairs_to_array(args.max_distance, segments, random_pairs, pairwise)
+
+    # Plots heatmap of array
+    heatmap(array, segments, args.max_distance, args.output)
